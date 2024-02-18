143915d148d240f9874a2c7e747b309f821574e2
vbrunste <vladimir.brunstein@hs-osnabrueck.de>
2024-02-18 19:51:32 +0100

    KeyPad schön gemacht

--- app/src/main/java/hsos/prog3/sudofun/View/KeyPad.java
+++ app/src/main/java/hsos/prog3/sudofun/View/KeyPad.java
@@ -4,10 +4,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import hsos.prog3.sudofun.model.Play;
 import hsos.prog3.sudofun.viewmodel.PlayActivity;
@@ -16,12 +12,16 @@ import hsos.prog3.sudofun.viewmodel.PlayGraphic;
 public class KeyPad {
     PlayActivity playActivity;
     Play game;
-
     PlayGraphic graphic;
     public KeyPad(PlayActivity playActivity, Play game, PlayGraphic graphic){
         this.playActivity = playActivity;
         this.game = game;
         this.graphic = graphic;
+        /**
+         * Registrieren des onClickListeners auf die Buttons im Tastenfeld
+         *
+         * @author C. Paul
+         */
         registerListener(playActivity.getBinding().numbOne);
         registerListener(playActivity.getBinding().numbTwo);
         registerListener(playActivity.getBinding().numbThree);
@@ -34,56 +34,63 @@ public class KeyPad {
         registerListener(playActivity.getBinding().clearButton);
 
     }
+    /**
+     * Funktion für das Registrieren des onClickListeners auf einen Button
+     * @author C. Paul
+     */
     private void registerListener(Button button){
         View.OnClickListener numbClickListener =  onClickListener(button,graphic,game);
         button.setOnClickListener(numbClickListener);
     }
-
+    /**
+     * Funktion für die Rückgabe der indizierten Notiz im GridLayout
+     *
+     * @author C. Paul
+     */
     private View getNote(int index,GridLayout noteGrid){
         return noteGrid.getChildAt(index-1);
     }
+
+    /**
+     * OnClickListener für alle Zahlen auf dem Tastenfeld
+     * je nach Modus wird der Inhalt des zuletzt fokussierten Views mit dem jeweiligen Zahlenwert überschrieben
+     *  bzw. gelöscht
+     * @author C. Paul
+     */
+
     public View.OnClickListener onClickListener(Button button,PlayGraphic graphic, Play game){
         return view -> {
             CharSequence buttonText = button.getText();
             EditText focusedEditText = graphic.getFocusedEditText();
             GridLayout focusedNoteGrid = graphic.getFocusedNoteGrid();
             View note = null;
+            /**
+             * Behandlung des Falls, bei dem der Notizmodus aktiviert ist
+             * @author C. Paul
+             */
             if(game.isNoteMode()){
-                switch(buttonText.toString()){
-                    case "1":
-                        note = getNote(1,focusedNoteGrid);
-                        break;
-                    case "2":
-                        note = getNote(2,focusedNoteGrid);
-                        break;
-                    case "3":
-                        note = getNote(3,focusedNoteGrid);
-                        break;
-                    case "4":
-                        note = getNote(4,focusedNoteGrid);
-                        break;
-                    case "5":
-                        note = getNote(5,focusedNoteGrid);
-                        break;
-                    case "6":
-                        note = getNote(6,focusedNoteGrid);
-                        break;
-                    case "7":
-                        note = getNote(7,focusedNoteGrid);
-                        break;
-                    case "8":
-                        note = getNote(8,focusedNoteGrid);
-                        break;
-                    case "9":
-                        note = getNote(9,focusedNoteGrid);
-                        break;
-                    case "clear":
-                        for(int i = 1; i<=9;i++){
-                            note = getNote(i,focusedNoteGrid);
-                            note.setVisibility(View.INVISIBLE);
-                        }
-                        return;
+                /**
+                 * löschen aller Notizen bei "clear" als Eingabe
+                 * @author C. Paul
+                 */
+                if(buttonText.toString().equals("clear")){
+                    for(int i = 1; i<=9;i++){
+                        note = getNote(i,focusedNoteGrid);
+                        note.setVisibility(View.INVISIBLE);
+                    }
+                    return;
                 }
+                else{
+                    /**
+                     * Parsen der Notiz, die getoggled werden soll
+                     * @author C. Paul
+                     */
+                    note = getNote(Integer.parseInt(buttonText.toString()),focusedNoteGrid);
+                }
+                /**
+                 * Toggeln der jeweiligen Notiz
+                 * @author C. Paul
+                 */
                 if(note != null) {
                     if (note.getVisibility() == View.INVISIBLE) {
                         note.setVisibility(View.VISIBLE);
@@ -92,11 +99,16 @@ public class KeyPad {
                     }
                 }
             }else{
-                if(!buttonText.toString().equals("clear")) {
-                    focusedEditText.setText(buttonText);
+                /**
+                 * Behandlung des Falls, bei dem der Notizmodus deaktiviert ist
+                 *
+                 * @author C. Paul
+                 */
+                if(buttonText.toString().equals("clear")) {
+                    focusedEditText.setText("");
                 }
                 else{
-                    focusedEditText.setText("");
+                    focusedEditText.setText(buttonText);
                 }
             }
         };
