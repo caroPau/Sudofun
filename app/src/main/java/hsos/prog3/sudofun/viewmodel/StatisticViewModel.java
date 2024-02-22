64a351a8f0b8898d6398ffd97d874d0082683fae
caropaul <carolin.paul@hs-osnabrueck.de>
2024-02-22 14:21:16 +0100

    StatisticViewModel

--- app/src/main/java/hsos/prog3/sudofun/viewmodel/StatisticViewModel.java
+++ app/src/main/java/hsos/prog3/sudofun/viewmodel/StatisticViewModel.java
@@ -1,12 +1,16 @@
 package hsos.prog3.sudofun.viewmodel;
 
+import static hsos.prog3.sudofun.model.Level.EASY;
+
 import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
+import hsos.prog3.sudofun.model.Level;
 import hsos.prog3.sudofun.model.Statistic;
 import hsos.prog3.sudofun.model.UserEntity;
 
@@ -26,11 +30,36 @@ public class StatisticViewModel extends AndroidViewModel {
         return statistic.getUser();
     }
 
+    public void setLevel(Level level){
+        statistic.setLevel(level);
+    }
+
+    public Level getLevel(){
+        return statistic.getLevel();
+    }
     public List<UserEntity> getBestUsers(){
         return statistic.getBestUsers();
     }
 
     public void setBestUsers(List<UserEntity> bestUsers){
         statistic.setBestUsers(bestUsers);
+    }
+
+    public LiveData<List<UserEntity>> getStatistics(DataViewModel dataViewModel) {
+        LiveData<List<UserEntity>> users ;
+        switch (getLevel()) {
+            case EASY:
+                users = dataViewModel.getHighscoresEasy();
+                break;
+            case MEDIUM:
+                users = dataViewModel.getHighscoresMedium();
+                break;
+            case HARD:
+                users = dataViewModel.getHighscoresHard();
+                break;
+            default:
+                users = null;
+        }
+        return users;
     }
 }
